e473ba1e1ee136a77f90a16e0f729aea095ab050
salman <salmanpatel453@gmail.com>
2020-07-05 01:41:14 +0530

    add files

---
+++ config-test/src/main/java/com/example/configtest/controller/ConfigTestController.java
@@ -0,0 +1,22 @@
+package com.example.configtest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class ConfigTestController {
+
+	@Value("${name}")
+	private String name;
+	
+	@GetMapping("/hello")
+	public String fetchName() {
+		
+		return name;
+	}
+	
+
+}
