46794ac03cdd4c4ee37f95abd1b60c4ff088fdea
salman <salmanpatel453@gmail.com>
2020-07-07 03:48:30 +0530

    refresh scope

--- config-test/src/main/java/com/example/configtest/controller/ConfigTestController.java
+++ config-test/src/main/java/com/example/configtest/controller/ConfigTestController.java
@@ -1,13 +1,14 @@
 package com.example.configtest.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RequestMapping("/api")
 @RestController
+@RefreshScope
 public class ConfigTestController {
 
     @Value("${message}")
