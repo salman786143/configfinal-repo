3ab489ee322a7fb757ec29c0b60b5e0b36b8f996
salman <salmanpatel453@gmail.com>
2020-07-06 14:55:21 +0530

    added

--- config-test/src/main/java/com/example/configtest/controller/ConfigTestController.java
+++ config-test/src/main/java/com/example/configtest/controller/ConfigTestController.java
@@ -1,18 +1,20 @@
 package com.example.configtest.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api")
 @RestController
+@RefreshScope
 public class ConfigTestController {
 
-    @Value("${message:Hello default}")
+    @Value("${message}")
 	private String name;
 	
-	@GetMapping("/hello")
+	@GetMapping("/name")
 	public String fetchName() {
 		
 		return name;
